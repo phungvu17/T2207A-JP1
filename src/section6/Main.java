76a217c0d3d09b08eaa14954a808b1e857c68972
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2023-02-01 13:47:55 +0700

    bai tap 2/1

---
+++ src/section6/Main.java
@@ -0,0 +1,15 @@
+package section6;
+
+public class Main {
+    public static void main(String[] args){
+        PhoneBook pb = new PhoneBook();
+        pb.insertPhone("NgoManhSon", "099999999");
+        pb.insertPhone("HaHuuHoang", "033333333");
+        pb.insertPhone("PhungVanVuu", "066666666");
+        pb.removePhone("NgoManhSon");
+        for(String pl : pb.PhoneList){
+            System.out.println(pl);
+        }
+
+    }
+}
