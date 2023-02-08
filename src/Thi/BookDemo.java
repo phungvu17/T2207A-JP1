4a8ee9dd6ed5d3086ed8ffc49932b092b57fbe20
phungvu17 <110510507+phungvu17@users.noreply.github.com>
2023-02-08 15:31:22 +0700

    Exam

---
+++ src/Thi/BookDemo.java
@@ -0,0 +1,12 @@
+package Thi;
+
+public class BookDemo {
+    public static void main(String[]args){
+     Author  author = new Author( "Winderand"," Russel ");
+     Book book = new Book("Developing Java Software","Russel Winderand",79.75);
+        System.out.println(book);
+
+    }
+
+
+}
